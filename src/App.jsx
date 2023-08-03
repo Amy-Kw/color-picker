4a02ce56fb5c408524e07bc9b3d9c7fb5d5b600a
Amy KWong <amykwong6@gmail.com>
2023-08-03 13:46:51 -0400

    update color pickers

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,65 @@
+import { useState } from 'react'
+
 // Write your Color component here
 
+
+const Color = ({color, setSelectedColor}) => {
+  return (
+    <div 
+      className={color} 
+      onClick={() => setSelectedColor(color)} 
+     ></div>
+  )
+}
+
+
+// const Color = ({color, setSelectedColor}) => { //<change the props to be
+ 
+//   return <div className={props.color}></div> //add in the "red" - props.color
+  
+// }
+
+//const [selectedColor, setSelectedColor] = useState("")
+
+// const Picker = () => {  < not this
+//   return(
+//     <div id="colors-list">
+//     <Color />
+//     <Color />
+//     <Color />
+//    </div>
+
+//   )
+// }
+
+// function Color ({#eb6b56, #462446, #1d4fda}) {
+//   retu
+// }
+
+
+
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("") //< added this here
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
+        {/* <div className="red">red</div> */}
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+
+
+      <div id="colors-list">{/* colors go here */}
+      <Color color='orange' setSelectedColor={setSelectedColor}/>
+      <Color color='violet' setSelectedColor={setSelectedColor}/>
+      <Color color='blue'   setSelectedColor={setSelectedColor}/>
+      </div>
     </div>
   );
 };
+
+
+
+
 
 export default App;
